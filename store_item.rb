70fa33265d6c0495f61af46a0daed1e54ee1e84d
markprubin <mark.p.rubin@gmail.com>
2022-10-25 12:12:22 -0700

    corrects price symbol on storeitem2

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,5 @@
-storeitem1 = {item: "Salsa", color: "red", price: 5, shelf_life: "2 months"}
-storeitem2 = {item: "Cheddar Cheese", color: "yellow", price 4, shelf_life: "2 weeks"}
-storeitem3 = {:item => "corn tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week"}
+storeitem1 = { item: "Salsa", color: "red", price: 5, shelf_life: "2 months" }
+storeitem2 = { item: "Cheddar Cheese", color: "yellow", price: 4, shelf_life: "2 weeks" }
+storeitem3 = { :item => "corn tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week" }
 
+puts "Pablo's food store has the best #{storeitem3[:color]} #{storeitem3[:item]} priced at #{storeitem3[:price]} dollars. Eat them quick, as their shelf life is only #{storeitem3[:shelf_life]}!"
