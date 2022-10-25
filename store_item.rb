045f815adba9045a27f5bc819ca526618625f20e
markprubin <mark.p.rubin@gmail.com>
2022-10-25 12:06:12 -0700

    changes hash name to generic and adds specific item as a key

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,4 @@
-salsa = {color: "red", price: 5, shelf_life: "2 months"}
-cheese = {color: "yellow", price 4, shelf_life: "2 weeks"}
-tortillas = {:color => "yellow", :price => 3, :shelf_life => "1 week"}
+storeitem1 = {item: "Salsa", color: "red", price: 5, shelf_life: "2 months"}
+storeitem2 = {item: "Cheese", color: "yellow", price 4, shelf_life: "2 weeks"}
+storeitem3 = {:item => "tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week"}
 
