aad07d93db747c90772e49d8c20908f16298e6c2
markprubin <mark.p.rubin@gmail.com>
2022-10-25 12:00:15 -0700

    init

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+salsa = {color: "red", price: 5, shelf_life: "2 months"}
+cheese = {color: "yellow", price 4, shelf_life: "2 weeks"}
+tortillas= = {:color => "yellow", :price => 3, :shelf_life => "1 week"}
+
