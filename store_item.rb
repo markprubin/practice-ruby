c6c7fe38dbafed478cfcca4cc55d7f031c9d9791
markprubin <mark.p.rubin@gmail.com>
2022-10-25 12:10:58 -0700

    added descriptives to items

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,4 @@
 storeitem1 = {item: "Salsa", color: "red", price: 5, shelf_life: "2 months"}
-storeitem2 = {item: "Cheese", color: "yellow", price 4, shelf_life: "2 weeks"}
-storeitem3 = {:item => "tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week"}
+storeitem2 = {item: "Cheddar Cheese", color: "yellow", price 4, shelf_life: "2 weeks"}
+storeitem3 = {:item => "corn tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week"}
 
