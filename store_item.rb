df6cc97d45848d3fbe34d438f555974e4f367b25
markprubin <mark.p.rubin@gmail.com>
2022-10-25 12:14:12 -0700

    adds statements

--- store_item.rb
+++ store_item.rb
@@ -3,3 +3,5 @@ storeitem2 = { item: "Cheddar Cheese", color: "yellow", price: 4, shelf_life: "2
 storeitem3 = { :item => "corn tortillas", :color => "yellow", :price => 3, :shelf_life => "1 week" }
 
 puts "Pablo's food store has the best #{storeitem3[:color]} #{storeitem3[:item]} priced at #{storeitem3[:price]} dollars. Eat them quick, as their shelf life is only #{storeitem3[:shelf_life]}!"
+
+puts "Pablo's food store has #{storeitem1[:color]} #{storeitem1[:item]} in stock, currently priced at #{storeitem1[:price]} dollars, with a shelf life of #{storeitem1[:shelf_life]}."
